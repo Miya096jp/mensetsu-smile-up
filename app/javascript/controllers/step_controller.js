785d9c04599c71cb987067a694db214116284176
Miya096jp <miyawaki.yske@gmail.com>
2026-06-18 17:28:04 +0900

    stepコントローラーで診断各ステップの画面開閉を制御

---
+++ app/javascript/controllers/step_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus";
+export default class extends Controller {
+  static targets = ["lp", "cameraCheck"]
+
+  show(e) {
+    this.currentStep = this[`${e.params.step}Target`]
+    this.currentStep.classList.remove("hidden")
+    this.lpTarget.classList.add("hidden")
+  }
+
+  close() {
+    this.currentStep.classList.add("hidden")
+    this.lpTarget.classList.remove("hidden")
+  }
+}
