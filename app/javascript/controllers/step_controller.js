c5eda48e241e080345aa7d55ab151ad4ac46d209
Miya096jp <miyawaki.yske@gmail.com>
2026-06-21 16:13:19 +0900

    各ステップ間の画面遷移が正しく行われるようにstep()、close()を修正

--- app/javascript/controllers/step_controller.js
+++ app/javascript/controllers/step_controller.js
@@ -1,17 +1,23 @@
 import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
-  static targets = ["lp", "cameraCheck"]
+  static targets = ["lp", "cameraCheck", "guide"]
   static outlets = ["camera-check"]
 
+  connect() {
+    this.currentStep = this.lpTarget
+  }
+
   show(e) {
-    this.currentStep = this[`${e.params.step}Target`]
-    this.currentStep.classList.remove("hidden")
-    this.lpTarget.classList.add("hidden")
+    const nextStep = this[`${e.params.step}Target`]
+    nextStep.classList.remove("hidden")
+    this.currentStep.classList.add("hidden")
+    this.currentStep = nextStep
   }
 
   close() {
     this.currentStep.classList.add("hidden")
     this.lpTarget.classList.remove("hidden")
+    this.currentStep = this.lpTarget
   }
 
   startCamera() {
