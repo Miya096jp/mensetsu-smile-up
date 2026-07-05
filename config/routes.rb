8877d25fb4c4e85f00d8d20308641169c26518e9
Miya096jp <miyawaki.yske@gmail.com>
2026-07-05 11:59:09 +0900

    利用規約ページとプライバシーポリシーページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root "diagnoses#new"
   resources :diagnoses, only: [ :create ]
+  get "terms", to: "pages#terms"
+  get "privacy", to: "pages#privacy"
 end
