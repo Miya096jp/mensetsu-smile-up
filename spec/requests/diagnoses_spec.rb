a0690ecad5549eb7c62e1a3b27dea7cec1fef40e
Miya096jp <miyawaki.yske@gmail.com>
2026-08-06 15:28:07 +0900

    バリデーションの呼び出しを実装

--- spec/requests/diagnoses_spec.rb
+++ spec/requests/diagnoses_spec.rb
@@ -1,20 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe "Diagnoses", type: :request do
+  include ActionDispatch::TestProcess::FixtureFile
+
   describe "POST /diagnoses" do
     it "returns ai diagnosis" do
       diagnosis = double()
       allow(diagnosis).to receive(:content).and_return("good impression")
+      photos = []
+      2.times do
+        photos << file_fixture_upload('photo.jpg', 'image/jpeg')
+      end
       allow(Llm::DiagnoseImpression).to receive(:call).and_return(diagnosis)
-      post "/diagnoses", params: { photos: [] }
+      post "/diagnoses", params: { photos: photos }
       expect(response.status).to eq 200
       hash = JSON.parse(response.body)
       expect(hash["content"]).to eq "good impression"
     end
 
+    it "returns 422 with invalid params" do
+      photos = [ file_fixture_upload('photo.jpg', 'image/jpeg') ]
+      post "/diagnoses", params: { photos: photos }
+      expect(response.status).to eq 422
+    end
+
     it "returns 500 and user message when api failed" do
       allow(Llm::DiagnoseImpression).to receive(:call).and_raise(RubyLLM::ServerError)
-      post "/diagnoses", params: { photos: [] }
+      photos = []
+      2.times do
+        photos << file_fixture_upload('photo.jpg', 'image/jpeg')
+      end
+      post "/diagnoses", params: { photos: photos }
       hash = JSON.parse(response.body)
       expect(response.status).to eq 500
       expect(hash["message"]).to eq "AI回答の取得に失敗しました"
