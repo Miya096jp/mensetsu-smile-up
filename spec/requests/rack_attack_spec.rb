16768e1e138cdd27d98cae2f8942d1f3b5739c12
Miya096jp <miyawaki.yske@gmail.com>
2026-08-13 18:37:27 +0900

    rack-attackのthrottlingのlimitを3に変更

--- spec/requests/rack_attack_spec.rb
+++ spec/requests/rack_attack_spec.rb
@@ -2,15 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "RackAttach", type: :request do
   describe "POST /diagnoses" do
-    it "not returns 429 within throttle limit" do
+    before do
+      Rack::Attack.enabled = true
       Rack::Attack.reset!
-      60.times { post "/diagnoses", params: { photos: [] } }
+    end
+
+    it "not returns 429 within throttle limit" do
+      3.times { post "/diagnoses", params: { photos: [] } }
       expect(response.status).not_to eq 429
     end
 
     it "returns 429 over throttle limit" do
-      Rack::Attack.reset!
-      61.times { post "/diagnoses", params: { photos: [] } }
+      4.times { post "/diagnoses", params: { photos: [] } }
       expect(response.status).to eq 429
     end
   end
